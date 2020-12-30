56b4dc00634b615e27e1a4fee9a16a387e94145f
Alberto Penzo <alberto.penzo.2@studenti.unipd.it>
2020-12-30 16:53:01 +0100

    reso Station abstract

--- station.h
+++ station.h
@@ -1,4 +1,18 @@
-class station
+#include <vector>
+
+class Station
 {
+public:
+    Station(const Station &) = delete;
+    Station &operator=(const Station &) = delete;
+    virtual ~Station() {}
+
+protected:
+    Station() {}
+
+private:
+    double distance_to_origin;
+    std::vector<bool> default_rail;
+
     //TODO
 };
